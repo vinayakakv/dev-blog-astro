8d6a216f2282f2ea95a301cf9e16a2f6e71d2737
Vinayaka K V <me.vinayakakv@gmail.com>
2025-07-19 12:56:17 +0530

    Seed Data

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,13 +5,11 @@ const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
-	schema: ({ image }) => z.object({
+	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: image().optional(),
+		date: z.coerce.date(),
+		tldr: z.string(),
 	}),
 });
 
